770b6bf4d3f3b4550919b1daa5995e9b68bd00c2
Linus Folkerts <dev@li-f.de>
2022-12-03 15:41:35 +0100

    Added more to streamlit scratch file

--- src/streamlit_scratch_lf.py
+++ src/streamlit_scratch_lf.py
@@ -1,21 +1,22 @@
 import streamlit as st
+import pandas as pd
+from PIL import Image
+
+# Load data
+sample_data = pd.read_json('data/sample_week.json')
 
 # Streamlit page config
 st.set_page_config(
     page_title="ECOmpute",
     layout="wide"
 )
+image_logo = Image.open('img/logo.png')
 
-col1, col2, col3 = st.columns(3)
 
-with col1:
-   st.header("A cat")
-   st.image("https://static.streamlit.io/examples/cat.jpg")
+layout_col1, layout_col2 = st.columns([1, 4])
 
-with col2:
-   st.header("A dog")
-   st.image("https://static.streamlit.io/examples/dog.jpg")
+with layout_col1:
+   st.image(image_logo)
 
-with col3:
-   st.header("An owl")
-   st.image("https://static.streamlit.io/examples/owl.jpg")
+with layout_col2:
+   st.write(sample_data)
