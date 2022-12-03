665d49cff4b7285408a5b89205dacff44cbec7c4
Lorenz Hufe <lorenz.hufe@web.de>
2022-12-03 21:30:20 +0100

    time chunk classification

---
+++ src/scheduler.py
@@ -0,0 +1,40 @@
+import pandas as pd
+
+columns_renewable = [
+    'Hydro water reservoir',
+    'Hydro pumped storage',
+    'Hydro Run-of-River',
+    'Wind offshore',
+    'Wind onshore',
+    'Solar',
+    'Geothermal',
+]
+
+columns_fossil = [    
+    'Others',
+    'Nuclear',
+    'Fossil brown coal / lignite',
+    'Fossil hard coal',
+    'Fossil oil',
+    'Fossil gas',
+    'Waste',
+    'Biomass',
+]
+
+def add_time_chunk_classification(data: pd.DataFrame):
+    """Adds the classification of green, grey or over-capacity timespan classifications to time series Dataframe."""
+
+    renewable_power = data[columns_renewable].sum(axis=1)
+    fossil_power = data[columns_fossil].sum(axis=1)
+
+    over_prodcution = (fossil_power + renewable_power) - data['Load']
+
+    data['Overproduction'] =  over_prodcution > 5000 
+    percentage_renewable = renewable_power / (fossil_power + renewable_power)
+
+    data['Green'] = (percentage_renewable > percentage_renewable.mean()) & (data['Overproduction'] == False)
+
+    # If not green and not overproduction, it is grey
+    data['Grey'] = (data['Green'] == False) & (data['Overproduction'] == False)
+    return data
+
