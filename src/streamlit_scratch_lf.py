9ff83c8a4b1307a15b4bcebd9b6d103099395979
Linus Folkerts <dev@li-f.de>
2022-12-03 15:27:57 +0100

    Added file for streamlit prototyping

---
+++ src/streamlit_scratch_lf.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+# Streamlit page config
+st.set_page_config(
+    page_title="ECOmpute",
+    layout="wide"
+)
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+   st.header("A cat")
+   st.image("https://static.streamlit.io/examples/cat.jpg")
+
+with col2:
+   st.header("A dog")
+   st.image("https://static.streamlit.io/examples/dog.jpg")
+
+with col3:
+   st.header("An owl")
+   st.image("https://static.streamlit.io/examples/owl.jpg")
