0cd0b7dc0d5ce27b8634e3ce3bbf56ac1b49ded1
Jarek <kerajli@yahoo.de>
2022-12-03 23:49:23 +0100

    Added FAST scheduling

--- src/scheduling.py
+++ src/scheduling.py
@@ -1,52 +1,14 @@
-import os
-import clingo
-from clingo import Function, Number
-
-class Job:
-    def __init__(self, id, length, deadline):
-        self.id = id
-        self.length = length
-        self.deadline = deadline
-    
-    def to_symbol(self):
-        return Function('', [Number(self.id), Number(self.length), Number(self.deadline)])
-
-class SchedulingContext:
-    def __init__(self, jobs, time_tags):
-        self._jobs = jobs
-        self._costs = [0] * len(time_tags)
-        current_costs = 0
-        for current_tag in 'excess', 'renewable', 'grey':
-            for i, tag in enumerate(time_tags):
-                if tag == current_tag:
-                    self._costs[i] = current_costs
-                    current_costs += 1
-
-    def job(self):
-        return [job.to_symbol() for job in self._jobs]
-
-    def horizon(self):
-        return Number(len(self._costs))
-
-    def costs(self, t):
-        return Number(self._costs[t.number])
+import pandas as pd
 
 
 def solve(jobs, time_tags):
-    ctl = clingo.Control(arguments=['--models=0'])
-    ctl.load(os.path.join('encodings', 'scheduling.lp'))
-    ctl.ground([('base', [])], context=SchedulingContext(jobs, time_tags))
+    jobs = jobs.sort_values(by='deadline')
 
-    models = []
-    with ctl.solve(yield_=True) as handle:
-        for model in handle:
-            models.append(model.symbols(atoms=True))
+    schedule = pd.Series(index=time_tags.index)
+    time_costs = time_tags.replace({'excess': 0, 'renewable': 1, 'grey': 2})
+    for job_id in jobs['id']:
+        prio_sorted_indices = time_costs[:jobs.loc[job_id,'deadline']]
+        schedule[prio_sorted_indices[:jobs.loc[job_id,'length']]] = job_id
+        time_costs.drop(prio_sorted_indices)
 
-    if len(models) == 0:
-        raise RuntimeError('No valid schedule was found.')
-
-    schedule = [None] * len(time_tags)
-    for s in models[-1]:
-        if s.match('schedule', 2):
-            schedule[s.arguments[0].number] = s.arguments[1].number
     return schedule
