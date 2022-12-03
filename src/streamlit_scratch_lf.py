43314fdd27b3ee3b1ae8c8293618fbe4b7760de8
Linus Folkerts <dev@li-f.de>
2022-12-03 15:51:34 +0100

    Added plot

--- src/streamlit_scratch_lf.py
+++ src/streamlit_scratch_lf.py
@@ -3,7 +3,9 @@ import pandas as pd
 from PIL import Image
 
 # Load data
-sample_data = pd.read_json('data/sample_week.json')
+sample_data = pd.read_json('data/sample_time_series.json')
+sample_data['xAxisValues'] = pd.to_datetime(sample_data['xAxisValues'], unit='ms')
+sample_data = sample_data.set_index('xAxisValues')
 
 # Streamlit page config
 st.set_page_config(
@@ -19,4 +21,4 @@ with layout_col1:
    st.image(image_logo)
 
 with layout_col2:
-   st.write(sample_data)
+   st.line_chart(sample_data)
